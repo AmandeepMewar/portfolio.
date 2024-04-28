113159a98530440a04757bfc203c6e313354db8b
Amandeep <amanmewar1718@gmail.com>
2024-04-28 22:37:05 +0530

    feat: add new project

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,14 +3,30 @@ import styles from "./Card.module.scss";
 import { IoLogoJavascript } from "react-icons/io5";
 import { FaReact, FaGithub, FaPython, FaCss3Alt } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
+import { SiTailwindcss } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { FaSass } from "react-icons/fa";
+
 const stackIcon = {
   ReactJs: <FaReact className={styles["card-icon"]} />,
 
-  Javascript: <IoLogoJavascript className={styles["card-icon"]} />,
+  JavaScript: <IoLogoJavascript className={styles["card-icon"]} />,
 
   Python: <FaPython className={styles["card-icon"]} />,
 
   CSS: <FaCss3Alt className={styles["card-icon"]} />,
+
+  TailwindCSS: <SiTailwindcss className={styles["card-icon"]} />,
+
+  TypeScript: <SiTypescript className={styles["card-icon"]} />,
+
+  NodeJs: <FaNodeJs className={styles["card-icon"]} />,
+
+  Redux: <SiRedux className={styles["card-icon"]} />,
+
+  Sass: <FaSass className={styles["card-icon"]} />,
 };
 
 const Card = props => {
--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,33 +1,67 @@
 const ProjectsData = [
   {
     id: 1,
-    title: "Game Space 🎮",
+    title: "Todo App 🗒️",
     description:
-      'This is a mini-project called "Game Space" that simulates an online game store. The application is developed using React and CSS Modules, and utilizes View Transitions API for smooth transitions, enhancing the user experience during navigation.',
-    stack: ["ReactJs", "Javascript", "CSS"],
+      "A Simple Todo App built using React, Sass and TypeScript. The Application UI is based on MacOS, while it uses your browsers local storage to store your Todos. This is my first work with typescript so as to bring the type safety and better developer experience into the picture.",
+    stack: ["ReactJs", "TypeScript", "Sass"],
+    code: "https://github.com/AmandeepMewar/Todo-App-TS",
+    demo: "https://wttodoappts.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "PostInks Notes App 📝",
+    description:
+      "A Full-Stack CRUD Application created using the tech stack React.js and Tailwind CSS for front-end while Node.js and Express.js for the backend and NoSQL database MongoDB. In the application user can create new posts, edit them and delete them if required.",
+    stack: [
+      "ReactJs",
+      "JavaScript",
+      "TailwindCSS",
+      "NodeJs",
+      "ExpressJs",
+      "MongoDB",
+    ],
+    code: "https://github.com/AmandeepMewar/PostInks-Notes-App",
+    demo: "https://postinksx-crud.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "RedXone E-commerce Game Store 🎮",
+    description:
+      "Developed an E-commerce platform for video games with React.js for easy shopping. The project is built using Redux Toolkit for state management. The application also features search functionality, cart functionality, and detailed game view page.",
+    stack: ["ReactJs", "JavaScript", "Sass", "Redux"],
     code: "https://github.com/AmandeepMewar/Game-space",
     demo: "https://gamespacex.vercel.app/",
   },
   {
-    id: 2,
+    id: 3,
+    title: "Game Space 🎮",
+    description:
+      'This is a mini-project called "Game Space" that simulates an online game store. The application is developed using React and CSS Modules, and utilizes View Transitions API for smooth transitions, enhancing the user experience during navigation.',
+    stack: ["ReactJs", "JavaScript", "CSS"],
+    code: "https://github.com/AmandeepMewar/Game-space",
+    demo: "https://gamespacex.vercel.app/",
+  },
+  {
+    id: 4,
     title: "Weather.io ⛅",
     description:
       "Weather.io is a simple React application designed to provide users with real-time weather information for a specified location. The app utilizes Material UI components with the Tomorrow.io real-time weather API to deliver accurate and up-to-date weather data.",
-    stack: ["ReactJs", "Javascript"],
+    stack: ["ReactJs", "JavaScript"],
     code: "https://github.com/AmandeepMewar/Weather.io",
     demo: "https://weatheriox.vercel.app/",
   },
   {
-    id: 3,
+    id: 5,
     title: "React Mini-Projects 💻",
     description:
       "This mini-project is a collection of commonly used functional components for web development, built with React and Styled Components.",
-    stack: ["ReactJs", "Javascript", "Styled-components"],
+    stack: ["ReactJs", "JavaScript", "Styled-components"],
     code: "https://github.com/AmandeepMewar/React-Mini-Projects",
     demo: "https://reactminiprojectx.netlify.app/",
   },
   {
-    id: 4,
+    id: 6,
     title: "Book Recommender System 📚",
     description:
       "Python notebook for Book Recommendation System, focused on implementing collaborative filtering techniques to provide personalized book recommendations based on user preferences.",
@@ -35,7 +69,7 @@ const ProjectsData = [
     code: "https://github.com/AmandeepMewar/Book-Recommender-System",
   },
   {
-    id: 5,
+    id: 7,
     title: "Credit Card Fraud Detection 💳",
     description:
       "This project employs machine Logistic regression, to enhance fraud detection in credit card transactions.",
@@ -43,7 +77,7 @@ const ProjectsData = [
     code: "https://github.com/AmandeepMewar/Credit-Card-Fraud-Detection",
   },
   {
-    id: 6,
+    id: 8,
     title: "Customer Segmentation 🛃",
     description:
       "This project uses clustering algorithms, like KMeans and DBSCAN, to group 200 people based on their behavior in a mall. Check out the code for a straightforward analysis of customer patterns and preferences.",
